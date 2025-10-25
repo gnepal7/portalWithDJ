8b7f27cb6be690eb9fc67e9b87e82abf4b798cf2
Shobhit Nepal <gnepal7@gmail.com>
2025-10-25 17:13:49 +0545

    Update settings.py
    Debug false on setting

--- djPortal/settings.py
+++ djPortal/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-lka7%r^a65anl_s#h*kkecjx$e)@^0($n8r&9s46#ge!b_w=3)
 # DEBUG = True
 # ALLOWED_HOSTS = []
 
-DEBUG = True
+DEBUG = False
 ALLOWED_HOSTS = ['portalwithdj.onrender.com', 'localhost', '127.0.0.1']
 
 # Application definition
@@ -113,13 +113,13 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 USE_TZ = True
 
-# STATIC_URL = '/static/'
-# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-# STATICFILES_DIRS =[BASE_DIR, 'static']
-
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATICFILES_DIRS =[BASE_DIR, 'static']
+
+# STATIC_URL = '/static/'
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
@@ -132,11 +132,11 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # CABINET_MEDIA_ROOT = BASE_DIR / 'media' / CABINET_MEDIA_PATH  
 # CABINET_MEDIA_URL = '/media/' + CABINET_MEDIA_PATH  
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR.parent, 'media')  
-CABINET_MEDIA_PATH = 'cabinet/'
-CABINET_MEDIA_ROOT = os.path.join(BASE_DIR.parent, 'media', CABINET_MEDIA_PATH)
-CABINET_MEDIA_URL = '/media/' + CABINET_MEDIA_PATH
+# MEDIA_URL = '/media/'
+# MEDIA_ROOT = os.path.join(BASE_DIR.parent, 'media')  
+# CABINET_MEDIA_PATH = 'cabinet/'
+# CABINET_MEDIA_ROOT = os.path.join(BASE_DIR.parent, 'media', CABINET_MEDIA_PATH)
+# CABINET_MEDIA_URL = '/media/' + CABINET_MEDIA_PATH
 
 TIME_ZONE = 'Asia/Kathmandu'
 USE_TZ = True
