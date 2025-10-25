0d469fb218f0706544d36034e93f602710318d09
Shobhit Nepal <gnepal7@gmail.com>
2025-10-25 19:12:09 +0545

    Update settings.py
    old style on static

--- djPortal/settings.py
+++ djPortal/settings.py
@@ -122,7 +122,6 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR.parent, 'staticfiles')
 # STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATICFILES_DIRS = [os.path.join(BASE_DIR.parent, 'static')]
-STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
