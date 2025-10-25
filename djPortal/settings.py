7e67d8072aaaafeeef7318826acc4b0291201112
Shobhit Nepal <gnepal7@gmail.com>
2025-10-25 19:02:19 +0545

    Update settings.py
    static file storage variable added in settings file.

--- djPortal/settings.py
+++ djPortal/settings.py
@@ -119,9 +119,10 @@ USE_TZ = True
 # STATICFILES_DIRS =[BASE_DIR, 'static']
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') 
+STATIC_ROOT = os.path.join(BASE_DIR.parent, 'staticfiles')
 # STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATICFILES_DIRS = [os.path.join(BASE_DIR.parent, 'static')]
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
