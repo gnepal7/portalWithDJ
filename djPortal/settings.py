092195d4109b0500f69f6b32472598f108c19394
gnepal7 <gnepapl7@gmail.com>
2025-10-26 10:16:39 +0545

    some changes on branch

--- djPortal/settings.py
+++ djPortal/settings.py
@@ -24,11 +24,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-lka7%r^a65anl_s#h*kkecjx$e)@^0($n8r&9s46#ge!b_w=3)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = True
-# ALLOWED_HOSTS = []
 
-DEBUG = False
-ALLOWED_HOSTS = ['portalwithdj.onrender.com', 'localhost', '127.0.0.1']
+# hosting setting for local server
+DEBUG = True
+ALLOWED_HOSTS = []
+
+# hosting setting for live server
+# DEBUG = False
+# ALLOWED_HOSTS = ['portalwithdj.onrender.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
@@ -123,8 +126,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# STATIC_URL = '/static/'
+# STATICFILES_DIRS =[BASE_DIR, 'static']
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
@@ -132,9 +140,7 @@ STATIC_URL = '/static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
-STATICFILES_DIRS =[
-  BASE_DIR, 'static'
-]
+
 
 MEDIA_ROOT = BASE_DIR /"media"
 MEDIA_URL = "/media/"
