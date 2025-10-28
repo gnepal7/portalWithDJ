e96f4b405307955a1313d6d4eeea3568dbc3df9f
gnepal7 <gnepal7@gmail.com>
2025-10-28 14:20:51 +0545

    date and time combined and format changed.

--- news/templatetags/custom_filters.py
+++ news/templatetags/custom_filters.py
@@ -1,50 +1,11 @@
-# from django import template
-
-# from django.templatetags.static import static
-
-# from datetime import datetime
-# import nepali_datetime
-
-# register = template.Library()
-
-# @register.filter
-# def multiply(value, arg):
-#     return value * arg
-
-# @register.filter
-# def nepali_date(value):
-#     if not value:
-#         return ""
-#     dt = value if isinstance(value, datetime) else value
-#     nep_dt = nepali_datetime.date.from_datetime_date(dt.date())
-#     nep_digits = str.maketrans("0123456789", "०१२३४५६७८९")
-#     month_map = {
-#         "Baisakh": "बैशाख", "Jestha": "जेठ", "Asar": "असार", "Shrawan": "साउन",
-#         "Bhadra": "भदौ", "Ashwin": "असोज", "Kartik": "कार्तिक", "Mangsir": "मंसिर",
-#         "Poush": "पुस", "Magh": "माघ", "Falgun": "फागुन", "Chaitra": "चैत"
-#     }
-#     day_map = {
-#         "Sunday": "आइतबार", "Monday": "सोमबार", "Tuesday": "मंगलबार", 
-#         "Wednesday": "बुधबार", "Thursday": "बिहीबार", "Friday": "शुक्रबार", 
-#         "Saturday": "शनिबार"
-#     }
-#     nep_month = month_map.get(nep_dt.strftime("%B"), nep_dt.strftime("%B"))
-#     nep_day = day_map.get(nep_dt.strftime("%A"), nep_dt.strftime("%A"))
-#     day = str(nep_dt.day).translate(nep_digits)
-#     year = str(nep_dt.year).translate(nep_digits)
-#     time = dt.strftime("%I:%M").translate(nep_digits)
-#     return f"{nep_month} {day}, {year}, {nep_day} {time}"
-
-
-# @register.filter
-# def news_image(news, default_image='images/default.jpg'):
-#     return news.news_image.file.url if news.news_image else static(default_image)
-
-
 from django import template
 from django.templatetags.static import static
-from datetime import datetime
+from datetime import datetime, timezone
 import nepali_datetime
+
+from django.utils import timezone
+from django.utils.timezone import localtime, is_naive, make_aware
+
 
 from news.models import Advertisement
 
@@ -55,49 +16,48 @@ def multiply(value, arg):
     return value * arg
 
 @register.filter
-def nepali_date(value):
-    if not value:
-        return ""
-    dt = value if isinstance(value, datetime) else value
+def nepali_date(value=None, current=False):
+    dt = datetime.now() if current or not value else value
+
+    if not isinstance(dt, datetime):
+        dt = datetime.combine(dt, datetime.min.time())
+
+    if isinstance(value, datetime) and not current:
+        if is_naive(value):
+            value = make_aware(value, timezone.get_default_timezone())
+        dt = localtime(value)
+    else:
+        if is_naive(dt):
+            dt = make_aware(dt, timezone.get_default_timezone())
+        dt = localtime(dt)
+
     nep_dt = nepali_datetime.date.from_datetime_date(dt.date())
     nep_digits = str.maketrans("0123456789", "०१२३४५६७८९")
+
     month_map = {
         "Baisakh": "बैशाख", "Jestha": "जेठ", "Asar": "असार", "Shrawan": "साउन",
         "Bhadra": "भदौ", "Ashwin": "असोज", "Kartik": "कार्तिक", "Mangsir": "मंसिर",
         "Poush": "पुस", "Magh": "माघ", "Falgun": "फागुन", "Chaitra": "चैत"
     }
     day_map = {
-        "Sunday": "आइतबार", "Monday": "सोमबार", "Tuesday": "मंगलबार", 
-        "Wednesday": "बुधबार", "Thursday": "बिहीबार", "Friday": "शुक्रबार", 
+        "Sunday": "आइतबार", "Monday": "सोमबार", "Tuesday": "मंगलबार",
+        "Wednesday": "बुधबार", "Thursday": "बिहीबार", "Friday": "शुक्रबार",
         "Saturday": "शनिबार"
     }
+
     nep_month = month_map.get(nep_dt.strftime("%B"), nep_dt.strftime("%B"))
     nep_day = day_map.get(nep_dt.strftime("%A"), nep_dt.strftime("%A"))
     day = str(nep_dt.day).translate(nep_digits)
     year = str(nep_dt.year).translate(nep_digits)
-    time = dt.strftime("%I:%M").translate(nep_digits)
-    return f"{nep_month} {day}, {year}, {nep_day} {time}"
 
+    hour_12 = dt.hour % 12
+    hour_12 = 12 if hour_12 == 0 else hour_12
+    minute = dt.minute
+    ampm_nep = "पूर्वाह्न" if dt.hour < 12 else "अपराह्न"
+    time = f"{hour_12:02d}:{minute:02d} {ampm_nep}".translate(nep_digits)
 
-@register.filter
-def current_nepali_date(value=None):
-    dt = datetime.now()
-    nep_dt = nepali_datetime.date.from_datetime_date(dt.date())
-    nep_digits = str.maketrans("0123456789", "०१२३४५६७८९")
-    month_map = {
-        "Baisakh": "बैशाख", "Jestha": "जेठ", "Asar": "असार", "Shrawan": "साउन",
-        "Bhadra": "भदौ", "Ashwin": "असोज", "Kartik": "कार्तिक", "Mangsir": "मंसिर",
-        "Poush": "पुस", "Magh": "माघ", "Falgun": "फागुन", "Chaitra": "चैत"
-    }
-    day_map = {
-        "Sunday": "आइतबार", "Monday": "सोमबार", "Tuesday": "मंगलबार", 
-        "Wednesday": "बुधबार", "Thursday": "बिहीबार", "Friday": "शुक्रबार", 
-        "Saturday": "शनिबार"
-    }
-    nep_month = month_map.get(nep_dt.strftime("%B"), nep_dt.strftime("%B"))
-    nep_day = day_map.get(nep_dt.strftime("%A"), nep_dt.strftime("%A"))
-    day = str(nep_dt.day).translate(nep_digits)
-    year = str(nep_dt.year).translate(nep_digits)
+    if value and not current:
+        return f"{nep_month} {day}, {year}, {nep_day} {time}"
     return f"{day} {nep_month} {year}, {nep_day}"
 
 
